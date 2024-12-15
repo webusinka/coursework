4b6e1f1bfa1935af99d27b2acf94f190523f0b6e
webusinka <veronikabykova05@gmail.com>
2024-12-15 14:03:49 +0700

    Style is working

--- journal/journal/settings.py
+++ journal/journal/settings.py
@@ -116,9 +116,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
+
+USE_I18N = True
 
 USE_I18N = True
 
