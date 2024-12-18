989b88989f63ba3f3a3039729e4735f33e431649
webusinka <veronikabykova05@gmail.com>
2024-12-18 18:28:50 +0700

    Added types students and teachers at registration

--- journal/accounts/views.py
+++ journal/accounts/views.py
@@ -11,7 +11,15 @@ def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save(commit=False)  # Не сохраняем пользователя сразу
+            user_type = request.POST.get('user_type')  # Получаем тип пользователя
+
+            if user_type == 'staff':
+                user.is_staff = True  # Устанавливаем флаг is_staff в True
+            else:
+                user.is_staff = False  # Устанавливаем флаг is_staff в False (по умолчанию)
+
+            user.save()  # Сохраняем пользователя
             messages.success(request, 'Вы успешно зарегистрированы! Теперь вы можете войти.')
             return redirect('login')  # Перенаправление на страницу входа
     else:
