6c9ad2a5e028f17ca6e2002819b7e6305388dada
webusinka <veronikabykova05@gmail.com>
2024-12-20 17:46:18 +0700

    The condition for the required group

--- journal/accounts/forms.py
+++ journal/accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 
 User  = get_user_model()
